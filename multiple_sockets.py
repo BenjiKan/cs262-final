131c4afda4637dbf2b3c727179870af03a9cd44d
BenjiKan <42553807+BenjiKan@users.noreply.github.com>
2023-04-25 18:57:55 -0400

    Create experiments for multiple sockets

---
+++ multiple_sockets.py
@@ -0,0 +1,141 @@
+import socket
+from threading import Thread
+from multiprocessing import Process, Queue
+import time
+
+import os
+from _thread import *
+
+    
+def init_machine(config):
+    HOST = str(config[0])
+    PORT = int(config[1])
+    print("starting server| port val:", PORT)#, "clock rate:", clock_rate)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
+        conn, addr = s.accept()
+        start_new_thread(consumer, (conn,addr,))
+        print("consumer thread started for connection", addr)
+
+def consumer(conn,addr):
+    # each machine listens on its own consumer thread, which initializes its queue
+    print("consumer accepted connection" + str(addr)+"\n")
+    while True: # TODO: change this condition so that when wants to kill the process, it can
+        data = conn.recv(1024)
+        dataVal = data.decode('ascii')
+        # msg_queue.append(dataVal)
+
+
+def producer(portVal1, portVal2):
+    # tries to initiate connection to another port
+
+    host = "127.0.0.1" # localhost
+    port_first = int(portVal1)
+    port_second = int(portVal2)
+    s1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    # sleepVal = 1.0/clock_rate
+
+    #sema acquire
+    try:
+        s1.connect((host,port_first))
+        print("Client-side connection success to port val:" + str(portVal1) + "\n")
+        s2.connect((host,port_second))
+        print("Client-side connection success to port val:" + str(portVal2) + "\n")
+
+        # while True:
+        #     # update clock value immediately
+        #     global clock_value
+        #     clock_value += 1
+
+
+        #     # if msg_queue is not empty, then read the first message in the queue
+        #     if len(msg_queue) > 0:
+        #         msg = msg_queue.pop(0)
+        #         # if msg is greater than clock_value, then update clock_value
+        #         if int(msg) > clock_value:
+        #             clock_value = int(msg)
+  
+        #         logger.info("msg received, logical clock time: "+str(clock_value)+" queue length: "+str(len(msg_queue)))
+
+        #     # if msg_queue empty, generate own event and follow instructions   
+        #     else:
+        #         producer_run_random_event(logger, s1, s2)
+            
+        #     # wait before next action
+        #     time.sleep(sleepVal)
+
+    
+
+    except socket.error as e:
+        print ("Error connecting producer: %s" % e)
+
+
+# clock_rate = random.randint(1, 6)
+# clock_value = 0
+# msg_queue = []
+# events_prob = 10
+
+def machine(config):
+    config.append(os.getpid())
+    # # initialize the logical clock value for the process
+    # global clock_value 
+    # clock_value = 0
+
+    # # initialize the queue for the process
+    # global msg_queue
+    # msg_queue = []
+
+    # # initialize clock rate for process
+    # global clock_rate
+    # clock_rate = random.randint(1, 6)
+
+    # # initialize the probability of an event
+    # global events_prob
+    # events_prob = 10
+
+    # # initialize the logging for the process
+    # global logger
+    # logger = setup_custom_logger('process'+str(config[1]-5550))
+    # logger.info("Log set up for process "+str(config[1]-5550)+" with clock rate "+str(clock_rate))
+    # # print("logging initialized for process", config[1]-5550)
+
+    # initialize listeners
+    init_thread = Thread(target=init_machine, args=(config,)) # start a thread for the consumer, to listen
+    init_thread.start()
+
+    #add delay to initialize the server-side logic on all processes
+    time.sleep(5)
+
+    # extensible to multiple producers, we just use one producer that connects to two threads though
+    prod_thread = Thread(target=producer, args=(config[2], config[3],)) # start a thread for the producer, to send
+    prod_thread.start()
+ 
+
+localHost= "127.0.0.1"
+    
+
+if __name__ == '__main__':
+    port1 = 5551
+    port2 = 5552
+    port3 = 5553
+    
+
+    # each config is structured as [host, listening port, sending port 1, sending port 2]
+    config1=[localHost, port1, port2, port3]
+    p1 = Process(target=machine, args=(config1,))
+    config2=[localHost, port2, port3, port1]
+    p2 = Process(target=machine, args=(config2,))
+    config3=[localHost, port3, port1, port2]
+    p3 = Process(target=machine, args=(config3,))
+    
+
+    p1.start()
+    p2.start()
+    p3.start()
+    
+    p1.join()
+    p2.join()
+    p3.join()
